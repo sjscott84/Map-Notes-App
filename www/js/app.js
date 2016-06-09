037e15ff1df8afbc9bb1ce298951a35616321450
Sarah Scott <s.j.scott84@gmail.com>
2016-06-09 15:06:56 -0700

    Add default option to search and minor style changes

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,25 @@
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('starter', ['ionic', 'ngCordova'])
 
+app.controller('LoadingCtrl', function($scope, $ionicLoading) {
+  $scope.show = function() {
+    $ionicLoading.show({
+      content: 'Loading',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
+    }).then(function(){
+       console.log("The loading indicator is now displayed");
+    });
+  };
+  $scope.hide = function(){
+    $ionicLoading.hide().then(function(){
+       console.log("The loading indicator is now hidden");
+    });
+  };
+});
+
 var map;
 
 app.value('listView', []);
