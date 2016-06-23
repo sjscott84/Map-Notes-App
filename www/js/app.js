9ccde3dc30746ad8f44ddce735fd91cbaba68d74
Sarah Scott <s.j.scott84@gmail.com>
2016-06-23 12:37:55 -0700

    Open navigation in new window if no map apps avaliable

--- www/js/app.js
+++ www/js/app.js
@@ -146,12 +146,12 @@ app.directive('info',['$cordovaAppAvailability', 'currentPlace', 'firebaseData',
     scope: {
       place:  '=places'
     },
-    template: '<div class="infowindow"><div class="iw-title">{{place.name}}</div><div class="iw-info"><p>Type: {{place.type}}</p><p>Note: {{place.note}}</p><a ng-click="openNewMap()"">View On Google Maps</a></div></div>',
+    template: '<div class="infowindow"><div class="iw-title">{{place.name}}</div><div class="iw-info"><p>Type: {{place.type}}</p><p>Note: {{place.note}}</p><a ng-click="openNewMap()"">Navigation</a></div></div>',
     link: function(scope, element, attrs) {
       scope.openNewMap = function(){
         var lat = currentPlace.lat;
         var lng = currentPlace.lng;
-        /*$cordovaAppAvailability.check('comgooglemaps://')
+        $cordovaAppAvailability.check('comgooglemaps://')
         .then(function(){
         var sApp = startApp.set('comgooglemaps://?q='+lat+'+'+lng+'&zoom=13');
         console.log("map avaliable")
@@ -161,7 +161,7 @@ app.directive('info',['$cordovaAppAvailability', 'currentPlace', 'firebaseData',
           alert(error);
         });
         })
-        .catch(function(){*/
+        .catch(function(){
           $cordovaAppAvailability.check('http://maps.apple.com')
           .then(function(){
             console.log('Apple map avaliable')
@@ -174,9 +174,9 @@ app.directive('info',['$cordovaAppAvailability', 'currentPlace', 'firebaseData',
           })
           .catch(function(){
             console.log("map not avaliable")
+            window.open("https://maps.google.com/maps?ll="+lat+","+lng+"&z=13&t=m&hl=en-US&q="+lat+"+"+lng, "_blank");
           })
-          //window.open("https://maps.google.com/maps?ll="+lat+","+lng+"&z=13&t=m&hl=en-US&q="+lat+"+"+lng);
-        //});
+        });
       }
     }
   }
