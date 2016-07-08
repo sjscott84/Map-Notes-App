48170011b6b4165a64683f58c2827d1f811d634a
Sarah Scott <s.j.scott84@gmail.com>
2016-07-08 14:07:24 -0700

    Fix login button issue

--- www/js/homeCtrl.js
+++ www/js/homeCtrl.js
@@ -1,6 +1,30 @@
 angular.module('starter')
   .controller('homeCtrl',['$scope', '$state', 'firebaseAuth', 'appState', 'user', 'popup', function($scope, $state, firebaseAuth, appState, user, popup){
 
+    $scope.activeUser = false;
+
+    $scope.$watchCollection(
+      function(){
+        return user.data;
+      },
+      function(newVal, oldVal){
+        if(newVal !== oldVal){
+          if(newVal){
+            $scope.activeUser = true;
+            $scope.displayName = newVal.displayName;
+          }else{
+            $scope.activeUser = false;
+          }
+        }else if(newVal === oldVal){
+          if(oldVal){
+            $scope.activeUser = true;
+            $scope.displayName = newVal.displayName;
+          }else{
+            $scope.activeUser = false;
+          }
+        }
+      },true);
+
     $scope.loginGoogle = function(){
       firebaseAuth.googleLogin(function(error){
         popup.loginErrors(error);
